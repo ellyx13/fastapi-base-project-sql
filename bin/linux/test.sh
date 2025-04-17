d622c416a390485314d8cdde2eb68237ecf46c64
ellyx13 <pltkiet@gmail.com>
2025-04-17 02:36:05 +0000

    ci: add GitHub Actions workflow for automated build and test process

--- bin/linux/test.sh
+++ bin/linux/test.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+set -e
+
 docker compose -f docker-compose-test.yml build
-docker compose -f docker-compose-test.yml up --abort-on-container-exit
+
+TEMP_LOG_FILE=$(mktemp)
+
+docker compose -f docker-compose-test.yml up --abort-on-container-exit 2>&1 | tee "$TEMP_LOG_FILE"
+
+if grep -qE "==+ .* failed" "$TEMP_LOG_FILE"; then
+  echo "❌ Some test cases failed!"
+  exit 1
+else
+  echo "✅ All tests passed."
+fi
+
 docker compose -f docker-compose-test.yml down -v
